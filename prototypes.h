09c888dee9bc304b9ec1a38bb346a4214c6357e2
Terex RH400 Mining Excavator <71463391+Terex-RH00-Mining-Excavator@users.noreply.github.com>
2023-08-13 12:39:36 -0700

    Create prototypes.h
    Up-to-date code

---
+++ prototypes.h
@@ -0,0 +1,32 @@
+#ifndef _PROTOTYPES_
+#define _PROTOTYPES_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#define FILE_NAMING_ERROR "File Naming Error."
+#define INVALID_FILE_ERROR "Invalid File Name."
+#define INDEX_ERROR "Index Out of Bounds Error."
+#define MEMORY_ERROR "Memory Error."
+#define SYNTAX_ERROR "Syntax Error."
+#define BYTE_ERROR "Byte Error."
+#define ERROR(str){								\
+		fprintf(stderr,"%s\n", str);     \
+		printf("ERROR: ARRAY NOT DUMPED.\n");	\
+		exit(0);								\
+		}										\
+
+typedef struct{
+  char beg, end;
+	size_t sz, opening, closing;
+	char* contents;
+}Token;
+
+Token Collect(const char*, char, char, size_t);
+
+void Basic_Syntax(char, char*, long*, long);
+
+void Complex_Syntax(char, char*, char**, long*, size_t*, long*);
+
+#endif
