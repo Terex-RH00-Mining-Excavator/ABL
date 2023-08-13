b05f7416616dd17f6009472d5d38cb8ae030fdfc
Terex RH400 Mining Excavator <71463391+Terex-RH00-Mining-Excavator@users.noreply.github.com>
2023-08-13 12:38:48 -0700

    Create main.c
    Up-to-date code

---
+++ main.c
@@ -0,0 +1,55 @@
+#include "PROTOTYPES.h"
+
+int main(int argc, char** argv){
+
+	clock_t time = clock();
+	
+ 	// File Input
+	if(!argv[1]){
+		ERROR(INVALID_FILE_ERROR);
+		return -1;
+	}
+	
+	FILE* File = fopen(argv[1], "r");
+	if(argv[2]) printf("\nOutput: \n");
+	
+	// COLLECT DATA
+	size_t index = 0, size = 2;
+	char* code = (char*)malloc(size);
+	for(char c; (c = fgetc(File)) != EOF; index++){
+		if(index > size-2) code = (char*)realloc(code, ++size);
+		code[index] = c;
+	} code[index] = '\n', code[++index] = '\0';
+  
+	// INTERPRET DATA
+  	long MCapacity = 0, StorageIndex = 0;
+	char *Storage = (char*)calloc(MCapacity,sizeof(char));
+	for(index = 0; code[index]; index++){
+		Basic_Syntax(code[index], Storage, &StorageIndex, MCapacity);
+		Complex_Syntax(code[index], code, &Storage, &StorageIndex, &index,&MCapacity);
+	}
+	free(code);
+	code = NULL;
+
+	// DATA
+	if(argv[2]){
+		printf(
+		"\n\nMEMORY: %li\nCUR_INDEX: %ld\nSPEED: %lds\n\nDATA INTERPRETATION:\n",
+		MCapacity,
+		StorageIndex,
+		clock()-time
+		);
+	for(size_t Data = 0; Data < MCapacity; Data++)
+		printf("ARRAY[%li]: DECIMAL: %d  |  CHARACTER: %c\n",
+			Data,
+			Storage[Data],
+			Storage[Data]
+		);
+  	}
+	free(Storage);
+	Storage = NULL;
+	if(!Storage && argv[2]) 
+		printf("DUMPED\n");
+	
+	return 0;
+}
